5a13c5e574d02b3d30e032179a48708a55a7a6c8
dundao <lijian572923574@gmail.com>
2026-05-19 22:02:23 +0800

    sit环境部署

--- bootstrap.js
+++ bootstrap.js
@@ -1,2 +1,7 @@
-const { Bootstrap } = require('@midwayjs/bootstrap');
-Bootstrap.run();
+const env = process.env.NODE_ENV || 'local'
+const dotenvPath = process.env.DOTENV_CONFIG_PATH || ('.env.' + env)
+require('dotenv').config({ path: dotenvPath })
+console.log('[bootstrap] NODE_ENV=' + env + ', dotenv=' + dotenvPath)
+
+const { Bootstrap } = require('@midwayjs/bootstrap')
+Bootstrap.run()
